bf83a83cf090cccc574f1dd32b56080e7bb5b81b
moyeongmin <qa98589858@gmail.com>
2022-10-23 14:43:36 +0900

    Initial commit

---
+++ app/src/main/java/com/example/forpractice/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.forpractice;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button nextbtn = findViewById(R.id.button);
+
+
+        nextbtn.setOnClickListener(new View.OnClickListener()
+        {
+
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this,secondactivity.class);
+                startActivity(i);
+
+            }
+        });
+}
+}
