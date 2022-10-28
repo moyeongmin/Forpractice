160099759b000e1ecabc5a3a4122b2a4b3791c85
moyeongmin <qa98589858@gmail.com>
2022-10-28 22:22:41 +0900

    ramotion circle menu

--- app/src/main/java/com/example/forpractice/MainActivity.java
+++ app/src/main/java/com/example/forpractice/MainActivity.java
@@ -1,11 +1,15 @@
 package com.example.forpractice;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+
+import com.ramotion.circlemenu.CircleMenuView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,6 +18,66 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Button nextbtn = findViewById(R.id.button);
+//        var items = document.querySelectorAll('.circle a');
+//
+//        for(var i = 0, l = items.length; i < l; i++) {
+//            items[i].style.left = (50 - 35*Math.cos(-0.5 * Math.PI - 2*(1/l)*i*Math.PI)).toFixed(4) + "%";
+//
+//            items[i].style.top = (50 + 35*Math.sin(-0.5 * Math.PI - 2*(1/l)*i*Math.PI)).toFixed(4) + "%";
+//        }
+//
+//        document.querySelector('.menu-button').onclick = function(e) {
+//            e.preventDefault(); document.querySelector('.circle').classList.toggle('open');
+//        }
+        final CircleMenuView menu = findViewById(R.id.circle_menu);
+        menu.setEventListener(new CircleMenuView.EventListener(){
+            @Override
+            public void onMenuOpenAnimationStart(@NonNull CircleMenuView view) {
+                Log.d("D","onMenuOpenAnimationStart");
+            }
+
+            @Override
+            public void onMenuOpenAnimationEnd(@NonNull CircleMenuView view) {
+                Log.d("D","onMenuOpenAnimationEnd");
+            }
+
+            @Override
+            public void onMenuCloseAnimationStart(@NonNull CircleMenuView view) {
+                Log.d("D","onMenuCloseAnimationStart");
+            }
+
+            @Override
+            public void onMenuCloseAnimationEnd(@NonNull CircleMenuView view) {
+                Log.d("D","onMenuCloseAnimationEnd");
+            }
+
+            @Override
+            public void onButtonClickAnimationStart(@NonNull CircleMenuView view, int index) {
+                Log.d("D","onButtonClickAnimationStart|index : "+index);
+            }
+
+            @Override
+            public void onButtonClickAnimationEnd(@NonNull CircleMenuView view, int index) {
+                Log.d("D","onButtonClickAnimationEnd|index : "+index);
+            }
+
+            @Override
+            public boolean onButtonLongClick(@NonNull CircleMenuView view, int buttonIndex) {
+                Log.d("D","onButtonLongClick|index : "+buttonIndex );
+                return false;
+            }
+
+            @Override
+            public void onButtonLongClickAnimationStart(@NonNull CircleMenuView view, int buttonIndex) {
+                Log.d("D","onButtonLongClickAnimationStart|index : "+buttonIndex );
+            }
+
+            @Override
+            public void onButtonLongClickAnimationEnd(@NonNull CircleMenuView view, int buttonIndex) {
+                Log.d("D","onButtonLongClickAnimationEnd|index : "+buttonIndex );
+            }
+        });
+
 
 
         nextbtn.setOnClickListener(new View.OnClickListener()
